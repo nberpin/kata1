588ed7c14d37233158338ea9ac0a323ca8d2fb18
nberpin <116831696+nberpin@users.noreply.github.com>
2025-02-20 19:38:20 +0000

    otro comentario

--- src/TotalPoints.java
+++ src/TotalPoints.java
@@ -8,6 +8,7 @@ public class TotalPoints {
             String[] parts = game.split(":"); // Divide el string en dos partes
             int x = Integer.parseInt(parts[0]); // Convierte la primera parte a entero
             int y = Integer.parseInt(parts[1]); // Convierte la segunda parte a entero
+            //pongo los dos gif seguidos
             if (x>y) {
                 total+=3;
             }
